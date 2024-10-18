a06bd24e0fffe9f1b3190e259694ba1030316a98
MaxHed <maxencehedouin@gmail.com>
2024-10-18 14:38:27 +0200

    add SEO

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,17 @@ import Products from '~/components/Products.vue'
 import Contact from '~/components/Contact.vue'
 import Footer from '~/components/Footer.vue'
 
+// add meta tags in french
+useHead({
+  title: 'Nobilis Coutellerie - Couteaux de Luxe Personnalisables et Artisanaux',
+  meta: [
+    { name: 'description', content: 'Découvrez Nobilis Coutellerie, créateur de couteaux pliants haut de gamme. Des couteaux personnalisables, faits main avec des matériaux rares, pour un luxe unique. Explorez nos collections : Essentiel, Signature, et Héritage.' },
+    { name: 'keywords', content: 'Nobilis Coutellerie, couteaux personnalisables, couteaux pliants, couteaux de luxe, artisanat français, couteaux haut de gamme, couteaux sur mesure, couteaux en édition limitée, gravures d\'artistes, matériaux rares, coutellerie de luxe' },
+  ],
+})
+
+
+
 const headerRef = ref(null)
 const sideMenuRef = ref(null)
 
